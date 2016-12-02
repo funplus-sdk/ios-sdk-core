4955547bdfbe0a218eca9c1eff4d2b519a22c301
yuankun.zhang <fmcocoa@gmail.com>
2016-12-02 12:32:00 +0800

    v4.0.4-alpha.2

--- Source/FunPlusSDK.swift
+++ Source/FunPlusSDK.swift
@@ -20,7 +20,7 @@ public class FunPlusSDK {
     
     // MARK: - Properties
     
-    public static let VERSION = "4.0.4-alpha.1"
+    public static let VERSION = "4.0.4-alpha.2"
     
     /// The singleton instance.
     static var instance: FunPlusSDK?
